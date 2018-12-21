38289c8dae489044a08dd03b5fd862940ea83457
Sebastian Bolanos <sebasheston@gmail.com>
2018-12-21 12:38:37 +0100

    Backup

--- dao.sh
+++ dao.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-
-endPoint='http://localhost:8545'
-DAO='0x00741602bB70270772D5Dc9D0Bd3Af29D1b78e9f'
-RinkebyDAO='0xdc98315ea5d929f17929a2f7adab3b26eeb1b2c9'
-from='0xb4124ceb3451635dacedd11767f004d8a28c6ee7'
+DAO='0x9787eC88D04074A915648Ae2daaB02d34D72B208'
+RinkebyDAO='0xBF6879f5f9AdD7D36b5c5B4f63E59fd4E32A6A1c'
 
 #althea.open.aragonpm.eth
-altheaId='33ec2d92fc47b5383901b8a3856d208d3a66104fa46c4a06f3f97536e9b6fc5b'
+altheaIdRin='33ec2d92fc47b5383901b8a3856d208d3a66104fa46c4a06f3f97536e9b6fc5b'
 # use this one for devchain
-#altheaId='6f154f8fd38a0dcc9fbfbb13f3a54845bbea75a4cfffa70a64c93579e1d257a7'
+altheaIdDev='6f154f8fd38a0dcc9fbfbb13f3a54845bbea75a4cfffa70a64c93579e1d257a7'
+
 CORE_NAMESPACE='c681a85306374a5ab27f0bbc385296a54bcd314a1948b6cf61c4ea1bc44bb9f8'
 APP_BASES_NAMESPACE='f1f3eb40f5bc1ad1344716ced8b8a0431d840b5783aea1fd01786bc26f35ac0f'
 APP_ADDR_NAMESPACE='d6f028ca0e8edb4a8c9757ca4fdccab25fa1e0317da1188108f7d2dee14902fb'
@@ -28,15 +26,15 @@ function getBlock() {
 EOF
 }
 
-#"data": "0x$getApp$APP_BASES_NAMESPACE$vaultId"
-function altheaAddress() {
+
+function localAlthea() {
   cat << EOF
 {
   "jsonrpc":"2.0",
   "method":"eth_call",
   "params":[{
     "to": "$DAO",
-    "data": "0x$getApp$APP_ADDR_NAMESPACE$financeId"
+    "data": "0x$getApp$APP_ADDR_NAMESPACE$altheaIdDev"
   },
   "latest"
   ],
@@ -45,15 +43,34 @@ function altheaAddress() {
 EOF
 }
 
+function altheaAddress() {
+  cat << EOF
+{
+  "jsonrpc":"2.0",
+  "method":"eth_call",
+  "params":[{
+    "to": "$RinkebyDAO",
+    "data": "0x$getApp$APP_ADDR_NAMESPACE$altheaIdRin"
+  },
+  "latest"
+  ],
+  "id":2
+}
+EOF
+}
 
-#url='http://localhost:8545'
 url='https://sasquatch.network/rinkeby'
 
-echo -n "Block Number "
-curl --silent -X POST --data "$(getBlock)" $endPoint | jq ".result"
-
-altheaAddress
+echo Local
 curl --silent \
   -H "Accept: application/json" \
  -H "Content-Type:application/json" \
- -X POST --data "$(altheaAddress)" $url
+ -X POST --data "$(localAlthea)" http://localhost:8545 | jq '.result'
+
+echo ""
+echo ""
+echo Rinkeby
+curl --silent \
+  -H "Accept: application/json" \
+ -H "Content-Type:application/json" \
+ -X POST --data "$(altheaAddress)" $url | jq '.result'
